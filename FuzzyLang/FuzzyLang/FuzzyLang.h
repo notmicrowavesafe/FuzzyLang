f63dfeec5c0b7f6049473cf58ddca18ea2a441c5
Tommy Buarque de Macedo <notmicrowavesafe@gmail.com>
2015-08-24 17:11:13 -0400

    Created Framework

---
+++ FuzzyLang/FuzzyLang/FuzzyLang.h
@@ -0,0 +1,19 @@
+//
+//  FuzzyLang.h
+//  FuzzyLang
+//
+//  Created by Tommy on 8/24/15.
+//  Copyright © 2015 FuzzyLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FuzzyLang.
+FOUNDATION_EXPORT double FuzzyLangVersionNumber;
+
+//! Project version string for FuzzyLang.
+FOUNDATION_EXPORT const unsigned char FuzzyLangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FuzzyLang/PublicHeader.h>
+
+
